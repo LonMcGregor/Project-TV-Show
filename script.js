53b38ef34f8b872dd22cda497f9f627e3514bcc9
l <l>
2026-03-18 11:29:52 +0000

    impl level 100

--- script.js
+++ script.js
@@ -7,6 +7,18 @@ function setup() {
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  for(let ep of episodeList){
+    document.writeln(`<article>
+    <h1>S${ep.season.toString().padStart(2,"0")} - E${ep.number.toString().padStart(2,"0")}
+    <h2>${ep.name}</h2>
+    <img src="${ep.image.medium}" />
+    <details>
+    <summary>blurb</summary>
+    ${ep.summary}
+    </details>
+    </article>`)
+  }
 }
+
 
 window.onload = setup;
