3b602229b851007fe7ab95d89c22a010ee640209
l <l>
2026-05-19 12:42:05 +0100

    fetch episodes dynamically

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 //You can edit ALL of the code here
-function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+
+let DATA = {};
+
+async function getAllEpisodes(){
+  DATA = await fetch(`https://api.tvmaze.com/shows/82/episodes`)
+  .then(body => body.json())
+}
+
+async function setup() {
+  await getAllEpisodes();
+  makePageForEpisodes(DATA);
 }
 
 function addSingleEp(ep){
@@ -19,7 +27,7 @@ function search(e){
     rootel.removeChild(child);
   })
   if(e.target.value.length > 0){
-    const allEpisodes = getAllEpisodes().filter(ep => ep.name.toLowerCase().indexOf(e.target.value.toLowerCase()) > -1);
+    const allEpisodes = DATA.filter(ep => ep.name.toLowerCase().indexOf(e.target.value.toLowerCase()) > -1);
     makePageForEpisodes(allEpisodes);
   } else {
     setup();
